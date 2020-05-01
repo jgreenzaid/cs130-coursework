cc95613bb3690e932994c9d8e244af44176e09a4
Jenna Greenzaid <jennagreenzaid@MacBook-Pro-3.fios-router.home>
2020-05-01 16:54:22 -0500

    I am posting my tutorial 4 files

---
+++ tutorial04/font-switcher/js/index.js
@@ -0,0 +1,14 @@
+const makeBigger = () => {
+   document.querySelector('.content').style.fontSize = '3.4em';
+   document.querySelector('.content').style.background = 'hotpink';
+   document.querySelector('.content').innerHTML = 'Hello!';
+};
+
+const makeSmaller = () => {
+  document.querySelector('.content').style.fontSize = '1.4em';
+  document.querySelector('.content').style.background = 'white';
+  document.querySelector('.content').innerHTML = 'Goodbye!';
+};
+
+document.querySelector('.a1').onclick = makeBigger;
+document.querySelector('.a2').onclick = makeSmaller;
---
+++ tutorial04/theme-switcher/js/index.js
@@ -0,0 +1,15 @@
+const defaultTheme = () => {
+  document.querySelector('#theme').className = "container";
+};
+
+const oceanTheme = () => {
+  document.querySelector('#theme').className = "container ocean";
+};
+
+const desertTheme = () => {
+  document.querySelector('#theme').className = "container desert";
+};
+
+document.querySelector('#default').onclick = defaultTheme;
+document.querySelector('#ocean').onclick = oceanTheme;
+document.querySelector('#desert').onclick = desertTheme;
